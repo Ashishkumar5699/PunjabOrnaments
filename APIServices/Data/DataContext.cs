53ad9767da80fbe5cf6ce6d9060035cbce2a4999
Ashish Kumar <ashish@qsstechnosoft.com>
2023-06-26 12:01:47 +0530

    API Service Added

---
+++ APIServices/Data/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using APIServices.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace APIServices.Data
+{
+    public class DataContext: DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<AppUser> Users { get; set; }
+    }
+}
