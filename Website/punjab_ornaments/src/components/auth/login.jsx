8fe1ed4ac0348c304b717bdbf6431abd22169a1e
Ashish <ashish@qssechnosoft.com>
2023-10-11 14:06:50 +0530

    Resolve conflicts and rebase the branch 3/8

--- Website/punjab_ornaments/src/components/auth/login.jsx
+++ Website/punjab_ornaments/src/components/auth/login.jsx
@@ -1,7 +1,43 @@
+import {
+    Box,
+    Card,
+    CardContent,
+    Typography,
+    CardActions,
+    Button,
+    TextField,
+    Stack,
+} from '@mui/material';
 import React from 'react'
 
+import { CenterComponent , FullScreantheComponent} from '../../assets/Style';
+
 export default function login() {
-  return (
-    <div>login</div>
-  )
+    const [Username, setUsername] = React.useState("");
+    const [Password, setPassword] = React.useState("");
+
+    return (
+        <Box sx={{ ...FullScreantheComponent, justifyContent:"center",backgroundColor: '#F2F2F2' }}>
+            <Card sx={{ ...CenterComponent }}>
+                <CardContent>
+                    <TextField
+                        label="User Name"
+                        variant="outlined"
+                        value={Username} />
+                    <br />
+                    <br />
+                    <TextField
+                        label="Password"
+                        variant="outlined"
+                        border="solid orange"
+                        value={Password} />
+                    <br />
+                    <br />
+                    <Button size="small">Login</Button>
+
+                </CardContent>
+            </Card>
+        </Box>
+
+    )
 }
--- Website/punjab_ornaments/src/routes/Router.jsx
+++ Website/punjab_ornaments/src/routes/Router.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom';
+import LoginPage from '../pages/LoginPage';
 
 export default function Router() {
     return (
         <Routes>
             <Route>
-                <Route path='/' element={<h1>hello</h1>} />
+                <Route path='/' element={<LoginPage/>} />
             </Route>
         </Routes>
     )
